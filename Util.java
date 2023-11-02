00f813cfbe8d7e66b0819eeadcb4b18a73dbaec2
fie7 <121331043+fie7@users.noreply.github.com>
2023-11-02 23:05:35 +1100

    Create Util.java

---
+++ Util.java
@@ -0,0 +1,12 @@
+package org.eclipse.swt.custom;
+
+import java.time.Duration;
+
+public class Util {
+    public static String humanReadableFormat(Duration duration) {
+        return duration.toString()
+                .substring(2)
+                .replaceAll("(\\d[HMS])(?!$)", "$1 ")
+                .toLowerCase();
+    }
+}
